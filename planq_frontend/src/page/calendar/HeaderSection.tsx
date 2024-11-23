ef13b07c6f67e6807de039c1a093d9160b9ddfa7
wongakim-99 <202021000@sangmyung.kr>
2024-11-24 00:12:59 +0900

    fix : "달력 컴포넌트 헤더 디자인 수정"
    달력 컴포넌트 헤더 디자인 일부 수정
    현재는 view 작업 먼저 간단히 진행해보고, 곧 천천히 TS코드 적용해서 기능 테스트 예정
    
    Related to : #1

--- planq_frontend/src/page/calendar/HeaderSection.tsx
+++ planq_frontend/src/page/calendar/HeaderSection.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
 import './HeaderSection.scss';
+import CalendarBackward from "../../assetss/calendar/CalendarBackward.svg"
+import CalendarFrontward from "../../assetss/calendar/CalendarFrontward.svg"
+
 
 const HeaderSection: React.FC = () => {
     return (
       <div className="header-section">
         <div className="header-section-month-navigation">
-          <div className="header-section-month-navigation-month-year">October, 2024</div>
-          <div className="header-section-month-navigation-nav-buttons">
-            <div className="header-section-month-navigation-nav-buttons-button">〈</div>
-            <div className="header-section-month-navigation-nav-buttons-button">〉</div>
+          <div className="header-section-month-navigation-month-year">
+            October, 2024
+            <div className="header-section-month-navigation-nav-buttons">
+              <div className="header-section-month-navigation-nav-buttons-button">
+                <img src={CalendarBackward} alt="Calendar Backward" />
+              </div>
+              <div className="header-section-month-navigation-nav-buttons-button">
+                <img src={CalendarFrontward} alt="Calendar Frontward" />
+              </div>
+            </div>
           </div>
+
           <div className="header-section-month-navigation-filter-buttons">
-            <button className="selected">전체</button>
+            <button className="header-section-month-navigation-filter-buttons-selected">
+              전체
+            </button>
             <button>일정</button>
             <button>할 일</button>
           </div>
         </div>
 
-        <div className="weekdays">
+        <div className="header-section-weekdays">
           {["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"].map((day) => (
-            <div key={day} className="weekday">
+            <div key={day} className="header-section-weekdays-weekday">
               {day}
             </div>
           ))}
